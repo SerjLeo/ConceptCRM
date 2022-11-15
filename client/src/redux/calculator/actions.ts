2952bc8baee2303d6a8d461db6981268a0e95dc9
sleontyev <sleontyev@ozon.ru>
2022-11-15 12:44:16 +0300

    change form method to get

--- client/src/redux/calculator/actions.ts
+++ client/src/redux/calculator/actions.ts
@@ -5,7 +5,7 @@ import AlertService from '../../utils/Alerts';
 const alertService = new AlertService()
 
 export const getResult = (form: Record<string, unknown>, url: string) => async (dispatch: Dispatch<CalcAction>) => {
-  const {result, errors} = await ApiService.apiRequest(`/calc/${url}`, form, 'POST')
+  const {result, errors} = await ApiService.apiRequest(`/calc/${url}`, form)
   if(errors) {
     alertService.push({type: 'error', message: 'Ошибка при расчетах'})
     dispatch({type: CalcActionTypes.CALC_ERROR})
